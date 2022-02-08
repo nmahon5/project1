63c44c40d004176f36e8f655e129b55322fde594
Nadya Mahoney <nadyamahoney@dhcp-vlan861-9-212-170.vlan861.client.wireless.uic.edu>
2022-02-08 16:26:13 -0600

    project improvements

--- noisesketch.js
+++ noisesketch.js
@@ -1,20 +1,35 @@
 function setup() {
     createCanvas(windowWidth,windowHeight);
     randomSeed(3);
-    for (let x = 0; x < 255; x++) {
-
-    }
+    
     
 }
 
+
+
 function draw() {
     background(0);
-    nofill();
-    stroke(255);
+    for (let x = 0; x < 255; x++) {
+
+    }
+let clr = 0.05;
+let r = noise(clr+100)*255;
+let g = noise(clr-50)*255;
+let b = noise(clr)*255;
+
+
+fill(r,g,b);
+rect(mouseX, mouseY, mouseX, mouseY);
+
+clr = clr + 1;
+
+
+    //nofill();
+    //stroke(255);
 
 
 
-
+/*
     beginShape();
     for (let i = 0; i < windowWidth; i++) {
         let y = windowHeight * random(0,1);
@@ -24,10 +39,10 @@ function draw() {
     }
     endShape();
 
+*/
 
 
-
-
+/*
 beginShape();
 for (let i = 0; i <windowWidth; i++) {
     var nx = map(i,0,windowWidth,0,10);
@@ -37,18 +52,6 @@ for (let i = 0; i <windowWidth; i++) {
 }
 endShape();
 
-
-
-
-r = noise(clr+100)*255;
-g = noise(clr-50)*255;
-b = noise(clr)*255;
-
-fill(r,g,b);
-//rect(windowWidth/2, windowHeight/2, 100, 100);
-
-clr = clr + 0.05;
-
-
+*/
 
 }
--- sketch.js
+++ sketch.js
@@ -1,35 +1,49 @@
 let blocks = [];
-let numberofblocks = 40;
-
-
+let value = 1 ;
+let numberofblocks = 0;
+let circleColor = 0;
+let lineColor = 0;
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //window.alert("this is my message");
+  circleColor = random(255); 
+  lineColor = random(255);
+  rectMode(CENTER);
 
-for(let i = 0; i <= numberofblocks; i++){
-  let x = i * 10;
-  let y = i * 10;
-  let speedX = 2;
-  let speedY = 2;
-  blocks[i] = new Block(x,y,speedX,speedY);
+
+  for(let i = 0; i <= numberofblocks; i++){
+    let x = i * 5;
+    let y = i * 5;
+    let speedX = 2;
+    let speedY = 2;
+    blocks[i] = new Block(x,y,speedX,speedY);
+  
+  }
+  
+
 }
-
-}
-
-
-
-
-
 
 function draw(){
-  background(0)
-  for(i = 0; i<blocks.length; i++){
-    blocks[i].move();
-    blocks[i].show();
+  background(0);
+
+
+  
+
+
+  for(t = 0; t<blocks.length; t++){
+    blocks[t].move();
+    blocks[t].show();
+  }
+
+  if (frameCount % 60 == 0) {
+  	circleColor = color(random(255), random(255), random(255));
+  	lineColor = color(random(255), random(255), random(255));
   }
 }
+
 
 class Block {
 
@@ -50,30 +64,89 @@ class Block {
   this.y = this.y + this.speedY;
     if(this.y >= windowHeight || this.y<=0 ){
     this.speedY = (-1)*this.speedY 
-
-}
   }
 
-  
-  
+
+  }
+
   
   show(){
     
 
-    noFill();
-stroke(175,185,265);
+noFill();
+stroke(lineColor);
+
 line(this.x, -1, -1, this.y);
 line(-1,this.x, this.y,-1);
-    
-circle((windowWidth*.75),(windowHeight*.75),this.x);
-    
-curve(1000, 2000, this.x, windowHeight, windowWidth, this.x, 2000, 1000);
 
-    
-    noFill();
-    stroke(255);
-    bezier(windowWidth, windowWidth, 0, windowHeight, windowHeight, 0, 100, 0, 0, 0, 100, 0);
-    bezier(windowHeight, 0, 0, 100, 100, 0, -windowHeight, 0, 0, 0, 100, 0);
+line(-this.x, windowHeight, windowWidth, -this.y);
+line(windowWidth,-this.x, -this.y,windowHeight);
+
+line((windowWidth*.75), (windowHeight*.75), this.y,1);
+line((windowWidth*.75), (windowHeight*.75), 1,this.x);
+
+noFill();
+stroke(circleColor);
+
+circle((windowWidth*.75), (windowHeight*.75), this.y);
+circle((windowWidth*.75), (windowHeight*.75), this.x);
 
   }
+}
+
+function mousePressed() {
+    circleColor = color(random(255), random(255), random(255));
+    lineColor = color(random(255), random(255), random(255));
+    strokeWeight(random(3));
+ }
+
+function keyPressed(){
+  if (keyCode === 65) {
+    value = 20;
+    numberofblocks = value ;
+
+    for(let i = 0; i <= numberofblocks; i++){
+      let x = i * 5;
+      let y = i * 5;
+      let speedX = 2;
+      let speedY = 2;
+      blocks[i] = new Block(x,y,speedX,speedY);
+    }
+
+  } else if (keyCode === 87){
+    value = 20;
+    numberofblocks = value ;
+
+    for(let i = 0; i <= numberofblocks; i++){
+      let x = i * 5;
+      let y = i * 5;
+      let speedX = 2;
+      let speedY = 2;
+      blocks[i] = new Block(x,y,speedX,speedY);
+    }
+
+  } else if (keyCode === 68){
+    value = 30;
+    numberofblocks = value ;
+
+    for(let i = 0; i <= numberofblocks; i++){
+      let x = i * 5;
+      let y = i * 5;
+      let speedX = 2;
+      let speedY = 2;
+      blocks[i] = new Block(x,y,speedX,speedY);
+    }
+
+  } else if (keyCode === 83){
+    value = 40;
+    numberofblocks = value ;
+    for(let i = 0; i <= numberofblocks; i++){
+      let x = i * 5;
+      let y = i * 5;
+      let speedX = 2;
+      let speedY = 2;
+      blocks[i] = new Block(x,y,speedX,speedY);
+    }
+
+  } 
 }
